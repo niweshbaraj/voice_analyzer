1d3cad517c799de6a732017962d8422160bf10a5
Niwesh Baraj <niweshbaraj@gmail.com>
2024-01-02 22:50:50 +0530

    removing flowbite from tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,12 @@
-import flowbitePlugin from 'flowbite/plugin';
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    'node_modules/flowbite-react/lib/esm/**/*.js',
   ],
   theme: {
     extend: {},
   },
-  plugins: [flowbitePlugin],
+  plugins: [],
 }
 
