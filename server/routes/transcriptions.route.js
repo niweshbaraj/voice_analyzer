1bf97a1f9e536d529ab17f170832915a28907d51
Niwesh Baraj <niweshbaraj@gmail.com>
2024-01-03 16:52:49 +0530

    fix server get to post

--- server/routes/transcriptions.route.js
+++ server/routes/transcriptions.route.js
@@ -5,10 +5,10 @@ import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
-router.get('/getTranscriptions/:id', verifyToken , getTranscriptions);
+router.post('/getTranscriptions/:id', verifyToken , getTranscriptions);
 router.post('/saveTranscription/:id', verifyToken, saveTranscription);
 router.delete('/deleteTranscription/:id/:t_id', verifyToken, deleteTranscription);
 
-router.get('/getStats/:id', verifyToken, transcriptionStats);
+router.post('/getStats/:id', verifyToken, transcriptionStats);
 
 export default router;
